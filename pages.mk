5d9414ce6ea6e959b49031b0819f1cc57f97992e
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2023-08-01 13:57:46 -0400

    Delete old Makefile fragment.

--- pages.mk
+++
@@ -1,4 +0,0 @@
-PAGES = index.md lojban/la-xunre-nunmro.md games/fall-of-plaguestone.md \
-        lojban/lxn-lujvo.md lojban/le-litru.md lojban/litru-lujvo.md \
-        lojban/rug-lujvo.md lojban/rug.md notes/origami/index.md \
-        notes/cpm.md
